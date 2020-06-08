9be13dc7f1c540a44ece091b1b493b3b5825717f
asala-hassan <60165523+asala-hassan@users.noreply.github.com>
2020-06-07 20:41:32 -0700

    Updating homework3

--- homework3/erlang/exercises.erl
+++ homework3/erlang/exercises.erl
@@ -1,29 +1,35 @@
--module (main).
-%-export([change/1]).
-change(amount) ->
 
-    quarters =(amount/25),
-	amount = amount rem 25,
-	  
-    dimes = (amount/10), 
-	amount = amount rem 10,
-	  
-	nickels = (amount/5),
-	  
-    pennies = amount rem 5, 
-	
-    [quarters, dimes, nickels, pennies].
-		
-	end.
-	
-    main(_) ->
-		io:format ("~s~n", change(5))
-	end.
+-module (exercises).
+-export([change/1]).
+-export([sumOfCubesOfOdds/1]).
+-export([powers/3]).
 
+change(Amount) ->
+    if Amount < 0 ->
+       {false, "amount cannot be negative"};
+    true ->
+       Quarters = Amount div 25,
+       AfterQuarters = Amount rem 25,
+       Dimes = AfterQuarters div 10,
+       AfterDimes = AfterQuarters rem 10,
+       Nickels = AfterDimes div 5,
+       Pennies = AfterDimes rem 5,
+       {true, [Quarters, Dimes, Nickels, Pennies]}
+    end.
+  
 
+sumOfCubesOfOdds(List)-> 
+  lists:sum([X*X*X || X <- List, X rem 2 =/= 0]).
 
+  
+powers(Base, Limit, Pid)->
+  helper(Base, Limit, Pid, 1).
 
-    
- 
+helper(Base, Limit, Pid, Power) when Power =< Limit ->
+  Pid ! Power,
+  helper(Base, Limit, Pid, Power * Base);
+  helper(_, _,  _, _) ->
+    ok.
+
 
 
